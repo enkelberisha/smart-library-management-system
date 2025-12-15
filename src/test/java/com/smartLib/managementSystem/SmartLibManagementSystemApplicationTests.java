c6e36b8722c7aad1ec7168f623e01be5a1944c2f
enkelberisha <99924910+enkelberisha@users.noreply.github.com>
2025-12-15 23:14:42 +0100

    Overwrite repo with local folder

---
+++ src/test/java/com/smartLib/managementSystem/SmartLibManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartLib.managementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartLibManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
